316134b9dcbb3bf84b2668abff368337851ca940
ltalavera19 <ltalavera@hawk.iit.edu>
2022-04-05 18:31:03 -0500

    done

---
+++ style/script.js
@@ -0,0 +1,18 @@
+function init(){
+//add your javascrip between these two lines of code
+ 
+var x1 = document.getElementById("entryinput").value;
+var x2 = document.getElementById("textoutput");
+
+alert("Lucien Talavera: "+ x1);
+x2.innerHTML = x1;
+
+}
+
+var x3 = document.getElementById('entrybutton');
+x3.addEventListener("click", alertFunction);
+
+
+
+}
+window.addEventListener('load', init);
